b6cea69df6c10eee069be5d44166a760bc617482
Svetozar Mateev <svetozar.mateev@tick42.com>
2022-05-20 11:10:53 +0300

    Added a groups react template

---
+++ groups-react/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Group from "@glue42/groups-ui-react";
+import "@glue42/groups-ui-react/dist/styles/styles.css";
+
+const App = () => {
+  return <Group />
+};
+
+export default App;
